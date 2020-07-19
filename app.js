e6e83582750c5ca9ff9c941963c832c047b29314
aaltwaim <a_t1411@hotmail.com>
2020-07-19 10:20:38 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,6 @@
+$(document).on("click",".sidebar-toggle",function(){
+    $(".wrapper").toggleClass("toggled");
+});
+// $(".btn.btn-primary.text-white").click(function(){
+//     $(".table.table-striped > tbody:last-child").append("<tr><td>7</td><td>7</td><td>7</td><td>7</td></tr>")
+// })
